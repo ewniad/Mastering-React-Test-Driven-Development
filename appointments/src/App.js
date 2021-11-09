aa1aba1093db883a427c0c6d4d1822e534c7e2bb
ewh <wkbyasi@gmail.com>
2021-11-09 23:29:38 +0900

    up

---
+++ appointments/src/App.js
@@ -0,0 +1,66 @@
+import React, { useState, useCallback } from 'react';
+import ReacctDOM from 'react-dom';
+//import { AppointmentFormLoader } from './AppointmentFormLoader';
+import { AppointmentsDayViewLoader } from './AppointmentsDayViewLoader';
+import { CustomerForm } from './CustomerForm';
+
+
+export const App = () => {
+  const [view, setView] = useState('dayView');
+//  const [customer, setCustomer] = useState();
+//
+//  const transitionToAddAppointment = useCallback(customer => {
+//    setCustomer(customer);
+//    setView('addAppointment');
+//  }, []);
+  const transitionToAddCustomer = useCallback(
+    () => setView('addCustomer'),
+    []
+  );
+//  const transitionToDayView = useCallback(
+//    () => setView('dayView'),
+//    []
+//  );
+//
+//  switch (view) {
+//    case 'addCustomer':
+//      return <CustomerForm onSave={transitionToAddAppointment} />;
+//    case 'addAppointment':
+//      return (
+//        <AppointmentFormLoader
+//          customer={customer}
+//          onSave={transitionToDayView}
+//        />
+//      );
+//    default:
+//      return (
+//        <React.Fragment>
+//          <div className="button-bar">
+//            <button
+//              type="button"
+//              id="addCustomer"
+//              onClick={transitionToAddCustomer}>
+//              Add customer and appointment
+//            </button>
+//          </div>
+//          <AppointmentsDayViewLoader />
+//        </React.Fragment>
+//      );
+//  }
+      return (
+        <React.Fragment>
+          <div className="button-bar" >
+            <button
+              type="button"
+              id="addCustomer"
+              onClick={transitionToAddCustomer}>
+              Add customer and appointment
+            </button>
+			</div>
+          <AppointmentsDayViewLoader />
+		  { view === 'addCustomer' ? <CustomerForm /> : null }
+        </React.Fragment>
+	);
+};
+
+
