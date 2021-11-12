3edcbeeda55ff7255b2c97223e76949784e14fdb
ewh <wkbyasi@gmail.com>
2021-11-12 11:04:19 +0900

    ok

--- appointments/src/CustomerSearch.js
+++ appointments/src/CustomerSearch.js
@@ -1,3 +1,66 @@
 import React from 'react';
 
-export const CustomerSearch = () => null;
+//export const CustomerSearch = () => null;
+
+export const CustomerSearch = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>First name</th>
+          <th>Last name</th>
+          <th>Phone number</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+	</table>
+  );
+};
+
+/*
+import React, { useEffect, useState } from 'react';
+
+const CustomerRow = ({ customer }) => (
+  <tr>
+    <td>{customer.firstName}</td>
+    <td>{customer.lastName}</td>
+    <td>{customer.phoneNumber}</td>
+    <td />
+  </tr>
+);
+
+export const CustomerSearch = () => {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await window.fetch('/customers', {
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json' }
+      });
+      setCustomers(await result.json());
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>First name</th>
+          <th>Last name</th>
+          <th>Phone number</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {customers.map(customer => (
+          <CustomerRow customer={customer} key={customer.id} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+*/
